465247602363db7bba5f735bd4ad9d01bd896675
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-03-22 00:24:09 -0300

    Menu principal con botones funcionales

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,44 @@
 import { FullScreen } from '@/components/FullScreen';
 import SWText from '@/components/SWText';
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-
-
+import SWButton from '@/components/SWButton';
+import { router } from 'expo-router';
+import { Image, StyleSheet, Platform, View, Text, Alert } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <FullScreen>
       <SWText>Index</SWText>
+      <View style={styles.menuContainer}>
+        <View>
+            <SWButton onPress={() =>
+                    router.replace('characters')
+                }>
+                PERSONAJES
+            </SWButton>
+        </View>
+        <View>
+            <SWButton onPress={() =>
+                     router.replace('planets')
+                 }>
+                PLANETAS
+            </SWButton>
+        </View>
+        <View>
+            <SWButton onPress={() =>
+                 router.replace('films')
+             }>
+                PELICULAS
+            </SWButton>
+        </View>
+      </View>
     </FullScreen>
   );
 }
+
+const styles = StyleSheet.create({
+    menuContainer:{
+        height:"85%",
+        justifyContent: "space-evenly", // Distribuye los elementos de manera uniforme
+        //backgroundColor:"red"
+    }
+})
