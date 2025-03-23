f92cf346ffebb8c0ac744f3c14328bb27fdec860
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-03-23 04:06:35 -0300

    orden y limpieza de código junto a ajuste de estilos

--- components/List.tsx
+++ components/List.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren, PropsWithRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {Alert,  StyleSheet, Text, View } from "react-native";
 import SWBox from "./SWBox";
 
 type Props = PropsWithRef<{}>
@@ -7,14 +7,19 @@ type Props = PropsWithRef<{}>
 function SWList({data}:Props){
 
     const CreateList = (data:any) =>{
-        var Element = [];
-        data.forEach((item:any)=>{
-            Element.push(
-                <SWBox>
-                    <Text style={styles.textElement}>{("name" in item)? item.name : item.title}</Text>
-                </SWBox>
-            )
-        })
+        let Element = [];
+        try{
+            data.forEach((item:any)=>{
+                Element.push(
+                    <SWBox key={("name" in item)? item.name : item.title}>
+                        <Text style={styles.textElement}>{("name" in item)? item.name : item.title}</Text>
+                    </SWBox>
+                )
+            })
+        }catch(error:any){
+            console.log(error)
+        }
+
         return Element;
     }
 
@@ -28,7 +33,9 @@ function SWList({data}:Props){
 const styles = StyleSheet.create({
   listContainer:{
     flexGrow: 1,
-    marginTop:45
+    marginTop:30,
+    marginBottom:10,
+    paddingBottom:50
   },
   textElement:{
     textAlign:"center",
