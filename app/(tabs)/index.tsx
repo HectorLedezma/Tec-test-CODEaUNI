a271600c2b82697c6731008d9e0714a6b219ea07
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-03-23 04:01:04 -0300

    orden y limpieza de código

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,11 +3,18 @@ import SWText from '@/components/SWText';
 import SWButton from '@/components/SWButton';
 import { router } from 'expo-router';
 import { Image, StyleSheet, Platform, View, Text, Alert } from 'react-native';
+//import FastImage from 'react-native-fast-image';
 
 export default function HomeScreen() {
+
   return (
     <FullScreen>
-      <SWText>Index</SWText>
+      <Image source={
+          {
+              uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png'
+          }
+      } style={styles.imageLogo} alt="logo"/>
+      <SWText>Enciclopedia</SWText>
       <View style={styles.menuContainer}>
         <View>
             <SWButton onPress={() =>
@@ -37,8 +44,13 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
     menuContainer:{
-        height:"85%",
+        height:"60%",
         justifyContent: "space-evenly", // Distribuye los elementos de manera uniforme
         //backgroundColor:"red"
-    }
+    },
+    imageLogo: {
+        width: "100%",
+        height: "25%",
+        justifyContent:"center"
+  },
 })
