8ff2a6cde7cea7f50bac67de8ce41200ec5cf27a
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-03-23 04:10:02 -0300

    problema con la traducción de los elementos

--- app/(tabs)/films.tsx
+++ app/(tabs)/films.tsx
@@ -1,30 +1,62 @@
+import { ScrollView, Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { useState, useEffect } from 'react'
 import { FullScreen } from '@/components/FullScreen';
 import SWText from '@/components/SWText';
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-import { useState, useEffect } from 'react'
-
-import Connection from '@/utils/ApiConnection.ts'
 import List from '@/components/List';
+import Connection from '@/utils/ApiConnection.ts'
+import Varios from '@/utils/Varios.ts'
 
 export default function Films() {
 
-    const [data,setData] = useState([]);
-    const [param, setParam] = useState("");
+    const utils = new Varios();
 
-    useEffect(()=>{
+    const [part,setPart] = useState("");
+
+    const [data,setData] = useState([]);
+
+    const translate = async (data:any[]) =>{
+
+        let final = [];
+        let results = data.results;
+        try{
+            await results.forEach((element)=>{
+                element.title = utils.EnToEs(element.title);
+                element.opening_crawl = utils.EnToEs(element.opening_crawl);
+                //console.log(element);
+                final.push(element);
+            });
+            data.results = final;
+        }catch(error:any){
+            console.log("\nError en función: ",error)
+        }
+        return data;
+    }
+
+    const update = (paramURI:string) =>{
         const con = new Connection();
-        con.getFilm(param).then(dataP=>{
-            setData(dataP.results);
+        con.getFilm(paramURI).then(dataP=>{
+            translate(dataP).then(dataT=>{
+                setData(dataT);
+            }).catch(error=>{
+                console.log("\nError en traducción: ",error);
+                setData([]);
+            });
         }).catch(error=>{
-            console.log(error);
+            console.log("Error en recuperación: ",error);
             setData([]);
         });
+    }
+
+    useEffect(()=>{
+        update(part)
     },[]);
 
   return (
     <FullScreen>
-      <SWText>Films</SWText>
-        <List data={data}/>
+      <SWText>Peliculas</SWText>
+      <ScrollView>
+        <List data={data.results}/>
+      </ScrollView>
     </FullScreen>
   );
 }
